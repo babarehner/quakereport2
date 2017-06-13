12fd0c93b61683fe8de180ab410daf14759c0149
Mike Rehner <babarehner@google.com>
2017-06-13 12:13:41 -0400

    Set LOG_TAGS for LoaderManager

--- app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -2,6 +2,7 @@ package com.example.android.quakereport;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.util.Log;
 
 import java.util.List;
 
@@ -11,23 +12,37 @@ import java.util.List;
 
 public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+    private String mUrl;
 
+    /**
+     * Constructs a new {@link EarthquakeLoader}
+     * @param context of the actiivity
+     * @param url to load date from
+     */
     public EarthquakeLoader(Context context, String url) {
         super(context);
-        // TODO: Finish implementing this constructor
+        Log.v(LOG_TAG, "Constructor, initLoader()");
+        mUrl = url;
     }
 
     @Override
     protected void onStartLoading() {
+        Log.v(LOG_TAG, "onStartLoading method");
         forceLoad();
     }
 
     @Override
     public List<Earthquake> loadInBackground() {
-        // TODO: Implement this method
-        // null from ctrl insert
-        return null;
-    }
+        Log.v(LOG_TAG, "loadInBackground method");
+        // Don't perform the request if there are no URLs, or the first URL is null.
+        // What happened to <if (mUrl.length == 0 || ) ????
+        if ( mUrl == null){
+            return null;
+        }
 
+        // List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return QueryUtils.fetchEarthquakeData(mUrl);
+    }
 
 }
